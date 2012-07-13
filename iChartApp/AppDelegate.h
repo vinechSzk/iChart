1366a245e9573a100f6a3e0278678378cc94a612
qtihb <yury.hb@gmail.com>
2012-07-13 11:01:54 +0800

    Initial Commit

---
+++ iChartApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iChartApp
+//
+//  Created by bin huang on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iChartApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iChartApp
+//
+//  Created by bin huang on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
